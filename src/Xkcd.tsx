02f1741bf74f19cd0ef4a4d035ef00c1bb86ee7f
Yikai Zhao <yikai@z1k.dev>
2023-04-09 16:54:00 +0800

    fixed xkcd in one day

--- src/Xkcd.tsx
+++ src/Xkcd.tsx
@@ -1,5 +1,11 @@
 import React, {useEffect} from 'react';
 
+
+const RandomWithSeed = function(seed: number) {
+  const x = Math.sin(seed) * 10000;
+  return x - Math.floor(x);
+};
+
 const Xkcd: React.FunctionComponent = () => {
   const [url, setUrl] = React.useState();
 
@@ -7,7 +13,9 @@ const Xkcd: React.FunctionComponent = () => {
     (async () => {
       const latest = await (await fetch('https://xkcd.now.sh/?comic=latest')).json();
       const latestNum = latest.num;
-      const randomNum = Math.floor(Math.random() * latestNum) + 1;
+
+      const daysSinceEpoch = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
+      const randomNum = Math.floor(RandomWithSeed(daysSinceEpoch) * latestNum) + 1;
       const json = await (await fetch('https://xkcd.now.sh/?comic=' + randomNum)).json();
       setUrl(json.img);
     })();
