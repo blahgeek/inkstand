a3ee30d9ed5234290b6d4ceb738dff80a4ea2d04
Yikai Zhao <yikai@z1k.dev>
2023-04-10 23:48:36 +0800

    cache xkcd api request

--- src/Xkcd.tsx
+++ src/Xkcd.tsx
@@ -5,6 +5,29 @@ const RandomWithSeed = function(seed: number) {
   return x - Math.floor(x);
 };
 
+const FetchJsonWithCache = async function(url: string, expireSec: number = 60 * 60 * 24) {
+  const cacheKey = 'fetch-cache-' + url;
+  const cache = localStorage.getItem(cacheKey);
+  if (cache) {
+    const cacheObj = JSON.parse(cache);
+    if (cacheObj.expiry > Date.now()) {
+      return cacheObj.data;
+    }
+  }
+
+  const data = await (await fetch(url)).json();
+  try {
+    localStorage.setItem(cacheKey, JSON.stringify({
+      data: data,
+      expiry: Date.now() + expireSec * 1000,
+    }));
+  } catch (e) {
+    console.log('failed to cache; clearing all storage', e);
+    localStorage.clear();
+  }
+  return data;
+};
+
 const Xkcd: React.FunctionComponent = () => {
   const [url, setUrl] = useState();
   const [alt, setAlt] = useState();
@@ -12,13 +35,13 @@ const Xkcd: React.FunctionComponent = () => {
   useEffect(() => {
     (async () => {
       // same as https://xkcd.com/info.0.json, but with CORS headers
-      const latest = await (await fetch('https://xkcd.now.sh/?comic=latest')).json();
+      const latest = await FetchJsonWithCache('https://xkcd.now.sh/?comic=latest');
       const latestNum = latest.num;
 
       // fixed image in one day
       const daysSinceEpoch = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
       const randomNum = Math.floor(RandomWithSeed(daysSinceEpoch) * latestNum) + 1;
-      const json = await (await fetch('https://xkcd.now.sh/?comic=' + randomNum)).json();
+      const json = await FetchJsonWithCache('https://xkcd.now.sh/?comic=' + randomNum);
       setUrl(json.img);
       setAlt(json.alt);
     })();
