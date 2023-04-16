657053c04d09025bb816f07c7bfd25a555ab846d
Yikai Zhao <yikai@z1k.dev>
2023-04-16 23:41:32 +0800

    use moment for date time

--- src/DateTime.tsx
+++ src/DateTime.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 const DateTime: React.FunctionComponent = () => {
-  // date like "Feb 2, Friday"
-  const date_str = new Date().toLocaleDateString('en-US', {
-    month: 'short',
-    day: 'numeric',
-    weekday: 'long',
-  });
+  // date like "Friday, Feb 2"
+  const date_str = moment().format('dddd, MMM D');
 
   // time like "20:30"
-  const time_str = new Date().toLocaleTimeString('en-US', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hourCycle: 'h23',
-  });
+  const time_str = moment().format('HH:mm');
 
   return (
     <div>
