62fcb5445690cb4830fb145472e85655f0b60bb8
Yikai Zhao <yikai@z1k.dev>
2025-05-17 10:55:45 +0800

    force timezone +8

--- src/DateTime.tsx
+++ src/DateTime.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import moment from 'moment';
 
 const DateTime: React.FunctionComponent = () => {
+  const now = moment().utcOffset(8);
+
   // date like "Friday, Feb 2"
-  const date_str = moment().format('dddd, MMM D');
+  const date_str = now.format('dddd, MMM D');
 
   // time like "20:30"
-  const time_str = moment().format('HH:mm');
+  const time_str = now.format('HH:mm');
 
   return (
     <div className="datetime">
